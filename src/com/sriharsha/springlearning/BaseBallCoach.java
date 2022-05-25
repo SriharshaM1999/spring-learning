9b3da5bc141fdd1d17a524c2e5b97c305f02dfd9
sriharsha <sriharsha@inncircles.com>
2022-05-25 21:01:40 +0530

    constructor injection and setter method injection in spring

--- src/com/sriharsha/springlearning/BaseBallCoach.java
+++ src/com/sriharsha/springlearning/BaseBallCoach.java
@@ -1,10 +1,25 @@
 package com.sriharsha.springlearning;
 
-public class BaseBallCoach implements Coach {
+public class BaseBallCoach implements CoachInterface {
+
+	HelperClass helperClass;
+	
+	// BaseBallCoach(HelperClass helperClass){
+	// 	this.helperClass = helperClass;
+	// }
 	
 	@Override
 	public void getDailyWorkOut() {
 		System.out.println("Dialy work out");
+	}
+
+	public void setIsHeGood(HelperClass helperClass){
+		this.helperClass = helperClass;
+	}
+
+	@Override
+	public void isHeGood() {
+		helperClass.isGoodCoach();
 	}
 
 }
